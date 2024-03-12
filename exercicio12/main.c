08c5fffcb67caa1260c579fcbaf4f787f6c548a0
Luiz Miguel <56977343+luizmiguelrosa@users.noreply.github.com>
2024-03-12 15:42:30 -0300

    Completando o exercício
    Função faltando no código

--- exercicio12/main.c
+++ exercicio12/main.c
@@ -10,10 +10,19 @@ int max(int *array, int tamanho) {
     return maior;
 }
 
+int min(int *array, int tamanho) {
+    int maior = array[0];
+    for (int i = 1; i < tamanho; i++) {
+        if (maior > array[i])
+            maior = array[i];
+    }
+    return maior;
+}
+
 int main() {
     int tamanho_array = 4;
     int *numeros_ptr = NULL;
-    int maior_numero;
+    int maior_numero, menor_numero;
 
     numeros_ptr = (int *) malloc(tamanho_array * sizeof(int));
     for (int i = 0; i < tamanho_array; i++) {
@@ -23,6 +32,9 @@ int main() {
 
     maior_numero = max(numeros_ptr, tamanho_array);
     printf("O maior numero foi %d\n", maior_numero);
+
+    menor_numero = min(numeros_ptr, tamanho_array);
+    printf("O menor numero foi %d\n", menor_numero);
 
     free(numeros_ptr);
     return 0;

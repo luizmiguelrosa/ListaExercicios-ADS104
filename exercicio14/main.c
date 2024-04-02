bf26043fc8f8e72dd7f48c06f39df2a8689f1db3
Luiz Miguel <56977343+luizmiguelrosa@users.noreply.github.com>
2024-04-02 16:29:31 -0300

    Melhorando o código
    Consertei alguns erros de lógica e completei com o exercício

--- exercicio14/main.c
+++ exercicio14/main.c
@@ -1,135 +1,46 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
-typedef struct {
-    char nome[50];
-    float valor_unitario;
-    int quantidade;
-} Item;
+void cria_espacamento(int tamanho) {
+    for (int i = 0; i < tamanho; i++)
+        printf(" ");
+}
 
-float pegar_valor_total(Item *itens, int *total_itens) {
-    float total = 0.0;
-    for (int i = 0; i < *total_itens; i++) {
-        total += itens[i].quantidade * itens[i].valor_unitario;
+int main(int argc, char const *argv[]) {
+    FILE *pessoas_arq;
+    char c, str_ax[100];
+    int index_ax = 0, i;
+    char * valores;
+
+    pessoas_arq = fopen("pessoas.txt", "r");
+    if (pessoas_arq == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return 1;
     }
-    return total;
-}
 
-int pegar_total_unidades(Item *itens, int *total_itens) {
-    int total = 0;
-    for (int i = 0; i < *total_itens; i++) {
-        total += itens[i].quantidade;
-    }
-    return total;
-}
+    printf("Nome            Idade\n");
+    while ((c = fgetc(pessoas_arq)) != EOF) {
+        if (c != '\n') {
+            str_ax[index_ax] = c;
+            index_ax++;
+        } else {
+            str_ax[index_ax] = '\0';
 
-void listar(Item *itens, int *total_itens) {
-    for (int i = 0; i < *total_itens; i++) {
-        printf("(%d) %s | R$%.2f | %d un\n", i+1, itens[i].nome, itens[i].valor_unitario, itens[i].quantidade);
-    }
-}
-
-void remover(Item *itens, int index, int *total_itens) {
-    for (int i = index; i < *total_itens - 1; i ++) {
-        itens[i] = itens[i + 1];
-    }
-    (*total_itens)--;
-}
-
-void cabecalho(char *texto) {
-    printf("\n-=-=-=-=-=-=- %s -=-=-=-=-=-=-\n", texto);
-}
-
-void erro(char *texto) {
-    printf("[ERRO] [ %s ]\n", texto);
-}
-
-void info(char *texto) {
-    printf("(SUCESSO) - %s\n", texto);
-}
-
-int main()
-{
-    int limite_itens, total_itens = 0, escolha, escolha_remover;
-    float valor_total;
-    Item *itens;
-
-    printf("Digite o limite total de itens: ");
-    scanf("%d", &limite_itens);
-    itens = (Item *) malloc(limite_itens * sizeof(Item));
-
-    while (true)
-    {
-        valor_total = pegar_valor_total(itens, &total_itens);
-        cabecalho("ADM Mercado");
-        printf("[%d] Itens          Valor total: R$%.2f\n\n", total_itens, valor_total);
-        printf("(1) Adicionar novo item\n(2) Remover um item\n(3) Relatorio dos itens\n(4) Sair\n\n> ");
-        scanf("%d", &escolha);
-
-        switch (escolha)
-        {
-        case 1:
-            if (total_itens < limite_itens) {
-                cabecalho("Novo Item");
-                printf("Nome do produto: ");
-                scanf(" %[^\n]", itens[total_itens].nome);
-
-                printf("Valor unitario: R$");
-                scanf("%f", &itens[total_itens].valor_unitario);
-
-                printf("Quantidade: ");
-                scanf("%d", &itens[total_itens].quantidade);
-
-                info("Item adicionado !");
-                total_itens ++;
-            } else {
-                erro("Limite de Itens Ocorrido");
+            i = 0;
+            valores = strtok(str_ax, ";");
+            while (valores != NULL) {
+                printf("%s      ", valores);
+                cria_espacamento(10 - strlen(valores));
+                valores = strtok(NULL, valores);
+                i++;
             }
-            break;
-        
-        case 2:
-            while (true)
-            {   if (total_itens > 0) {
-                    cabecalho("Remover Item da Compra");
-                    listar(itens, &total_itens);
 
-                    printf("\n> ");
-                    scanf("%d", &escolha_remover);
-
-                    if (escolha_remover > 0 && escolha_remover <= total_itens) {
-                        remover(itens, escolha_remover - 1, &total_itens);
-                        break;
-                    } else
-                        erro("Opcao Invalida");
-                } else {
-                    erro("Sem itens para remover");
-                    break;
-                }
-            }
-            break;
-        
-        case 3:
-            if (total_itens > 0) {
-                cabecalho("Relatorio dos Itens");
-                listar(itens, &total_itens);
-
-                int total_unidades = pegar_total_unidades(itens, &total_itens);
-                printf("\nUnidades: %d    Valor total: R$%.2f\n", total_unidades, valor_total);
-            } else {
-                erro("Sem itens para gerar Relatorio");
-            }
-            break;
-        
-        case 4:
-            free(itens);
-            return 0;
-
-        default:
-            break;
+            printf("\n");
+            
+            index_ax = 0;
         }
     }
-
+    fclose(pessoas_arq);
     return 0;
 }
